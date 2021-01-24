1dbfa9c21c21843c308d900f6fdcc03f71138d2f
Natanael Silva <natan.progjs@gmail.com>
2021-01-24 00:39:39 -0300

    creating password encryption

--- src/app/validators/user.js
+++ src/app/validators/user.js
@@ -6,7 +6,10 @@ async function post(req, res, next) {
 
     for(key of keys) {
         if (req.body[key] == "") { 
-            return res.send("Please, fill all fields!")
+            return res.render('user/register',  {
+                user: req.body,
+                error: 'Por favor, preencha todos os campos.'
+            })
         }
     }
 
@@ -20,12 +23,21 @@ async function post(req, res, next) {
         or:{ cpf_cnpj }
     }) 
 
-    if(user) return res.send('User exists')
+    if(user) {
+        return res.render('user/register',  {
+            user: req.body,
+            error: 'Usuário já cadastrado.'
+        })
+    } 
 
     // check if password match
-    if(password != passwordRepeat) 
-        return res.send('Password Mismatch') 
-    
+    if(password != passwordRepeat) {
+        return res.render('user/register',  {
+            user: req.body,
+            error: 'A senha e a repetição da senha estão incorretas.'
+        })
+    }
+
     next()
 }
 
