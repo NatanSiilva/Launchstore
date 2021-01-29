daea5bc498aff05d9a9782d1bd72a22b3560fb70
Natanael Silva <natan.progjs@gmail.com>
2021-01-29 15:03:01 -0300

    Blocking and redirecting routes finaization

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,9 +7,10 @@ const UserController = require('../app/Controllers/UserController')
 const UserValidator = require('../app/validators/user')
 const SessionValidator = require('../app/validators/session')
 
+const { isLoggedRedirectToUsers, onlyUsers } = require('../app/middlewares/session')
 
 // // login/logout
-routes.get('/login', SessionController.loginForm)
+routes.get('/login', isLoggedRedirectToUsers, SessionController.loginForm)
 routes.post('/login', SessionValidator.login, SessionController.login)
 routes.post('/logout', SessionController.logout)
 
@@ -25,7 +26,7 @@ routes.get('/register', UserController.registerForm)
 routes.post('/register', UserValidator.post, UserController.post)
 
 
-routes.get('/', UserValidator.show, UserController.show)
+routes.get('/', onlyUsers, UserValidator.show, UserController.show)
 routes.put('/', UserValidator.update, UserController.update)
 // routes.delete('/', UserController.delete)
 
