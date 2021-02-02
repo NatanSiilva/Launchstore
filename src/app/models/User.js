2919ea9daffe83986cd44c4120cf029691dfcd63
Natanael Silva <natan.progjs@gmail.com>
2021-02-02 12:30:36 -0300

    finalization Advanced exclusion logic

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,5 +1,7 @@
 const db = require('../../config/db')
 const { hash } = require('bcryptjs')
+const fs = require('fs')
+const Product = require('../models/Product')
 
 
 module.exports = {
@@ -79,5 +81,29 @@ module.exports = {
 
         await db.query(query) 
         return
+    },
+
+    async delete(id) {
+        // pegar todos os produtos
+        let results = await db.query("SELECT * FROM products WHERE user_id = $1", [id])
+        const products = results.rows
+
+        // dos produtos, pegar todas as imagens
+        const allFilePromise = products.map(product => Product.files(product.id))
+        let promiseResults = await Promise.all(allFilePromise)
+
+        // rodar a remoção do usuário
+        await db.query('DELETE FROM users WHERE id = $1', [id])
+
+        // remover as imagens da pasta public
+        promiseResults.map(results => {
+            results.rows.map(file => { 
+                try {
+                    fs.unlinkSync(file.path)
+                } catch (error) {
+                    console.error(error)
+                } 
+            })
+        })
     }
 }
