24d3f7f7cc3df953f2ac79f4a520964c7ff9a768
Natanael <natanaelsilva.progjs@gmail.com>
2021-11-26 23:08:44 -0300

    fix: script

--- public/script.js
+++ public/script.js
@@ -307,3 +307,38 @@ const Validate = {
     }
 
 }
+
+
+
+
+export function Timer(callback, delay) {
+   var timerId;
+   var start;
+   var remaining = delay;
+
+   this.pause = function () {
+      window.clearTimeout(timerId);
+      remaining -= new Date() - start;
+   };
+
+   var resume = function () {
+      start = new Date();
+      timerId = window.setTimeout(() => {
+         remaining = delay;
+         resume();
+         callback();
+      }, remaining);
+   };
+   this.resume = resume;
+
+   this.reset = function () {
+      remaining = delay;
+   };
+}
+
+var timer = new Timer(function () {
+   console.log("Chamou a função!");
+}, 3000);
+timer.resume();
+
+
